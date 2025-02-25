b79bd893d94b249e01a657d8e7283143283661a5
Mahdi`PHi <121295421+mahdiphi@users.noreply.github.com>
2025-02-25 18:54:40 +0330

    2

--- 8. Stacks/stc.cpp
+++ 8. Stacks/stc.cpp
@@ -1,30 +1,84 @@
 #include <iostream>
-#include <String>
 #include <vector>
 using namespace std;
 
-vector<string> nameList;
-
-class info {
-    string name;
-
-    void push(){
-        
-    }
-};
+struct ids
+{
+    vector<int> queue;
+    vector<int> stacks;
+} Ids;
 
 void stacks()
 {
-    string name;
-    cout << "Enter your name: ";
-    getline(cin, name);
+    int choice, num;
+    cout << "\n**********stacks storage**********\n";
+    cout << "1. Add\n";
+    cout << "2. Delete\n";
+    cout << "3. Show\n";
+    cout << "Enter your choice:\n";
+    cin >> choice;
 
-
+    switch (choice)
+    {
+    case 1:
+        num = Ids.stacks.size() + 1;
+        Ids.stacks.push_back(num);
+        break;
+    case 2:
+        Ids.stacks.pop_back();
+        break;
+    case 3:
+    {
+        if (Ids.stacks.empty())
+        {
+            cout << "there are no stacks here;";
+            break;
+        }
+        int show = Ids.stacks.back();
+        cout << "The stacks are: " << show;
+    }
+    break;
+    default:
+        cout << "wrong choice!";
+        break;
+    }
 }
 
 void queue()
 {
+    while (true)
+    {
 
+        int choice, num;
+        cout << "\n**********queue storage**********\n";
+        cout << "1. Add\n";
+        cout << "2. Delete\n";
+        cout << "3. Show\n";
+        cout << "0. Exite\n";
+        cout << "Enter your choice:\n";
+        cin >> choice;
+        if (choice == 0)
+            break;
+
+        switch (choice)
+        {
+        case 1:
+            Ids.queue.push_back(Ids.queue.size() + 1);
+            break;
+        case 2:
+            Ids.queue.pop_back();
+            break;
+        case 3:
+            for (int j = 0; j < Ids.queue.size(); j++)
+            {
+                cout << Ids.queue[j] << ", ";
+            }
+            break;
+        default:
+            cout << "wrong choice!";
+            break;
+        }
+    }
 }
 
 int main()
@@ -55,7 +109,7 @@ int main()
             break;
 
         default:
-        cout << "Error: wrong choice! Please enter 1, 2 or 0.\n";
+            cout << "Error: wrong choice! Please enter 1, 2 or 0.\n";
             break;
         }
     }
